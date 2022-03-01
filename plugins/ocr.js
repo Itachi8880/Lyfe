12105497475a97e572d42399c52752e510841363
ᴀɴꜱʜɪᴅ <99322970+AnshidZx7@users.noreply.github.com>
2022-03-01 18:29:41 +0530

    Update ocr.js

--- plugins/ocr.js
+++ plugins/ocr.js
@@ -1,4 +1,5 @@
-/* Copyright (C) 2020 Yusuf Usta.
+
+ /* Copyright (C) 2020 Yusuf Usta.
 
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
@@ -42,7 +43,8 @@ Asena.addCommand(
 
     return await message.reply(Lang.RESULT.format(dil[2], result))
   }
-)
+) 
+
 
 
 
